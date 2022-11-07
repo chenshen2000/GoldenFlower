1392326d307eaa6506a64298dd6ef6d73ba6be93
chenshen2000 <2975650439cjk>
2022-11-07 20:30:26 +0800

    进度条问题修复

--- range.js
+++ range.js
@@ -1,32 +1,29 @@
-$("body").append($("<div class='loading'><img src='./扑克牌图片/load.gif'><div><div></div><span>图片资源加载(<span>0/55</span>)</span></div></div>"));
+$("body").prepend($("<div class='loading'><div><img src='./扑克牌图片/load.gif'></div><div><div></div><span>图片资源加载(<span>0/55</span>)</span></div></div>"));
 $(".loading").css({
     "background-color":"rgb(55,195,230)",
     "position":"fixed",
     "top":"0","bottom":"0",
-    "left":"0","right":"0"
-}).children("img").css({
+    "left":"0","right":"0",
+    "z-index":"1"
+}).children("div:eq(0)").css("text-align","center").children("img").css({
     "width":"300px",
-    "position":"absolute",
-    "top":"10%",
-    "left":"50%","margin-left": "-150px",
-}).end().children("div").css({
+}).end().end().children("div:eq(1)").css({
     "width":"300px","height":"50px",
     "border":"1px solid green","border-radius":"25px",
-    "position":"absolute",
-    "top":"60%",
-    "left":"50%","margin-left": "-150px",
+    "margin":"auto",
+    "overflow":"hidden"
 }).children("div").css({
     "width":"0px","height":"50px",
     "background-color":"pink",
     "border-radius":"25px",
 }).next().css({
     "position":"absolute",
-    "top":"100%",
-    "left":"30%",
+    "left":"50%",
+    "margin-left":"-71px"
 })
 var images = new Array();
 var queue=[];
-// var queue=[29742, 30822, 30822, 37373, 42757, 44073, 44750, 45785, 47169, 48521, 49234, 50537, 51954, 52717, 53425, 54123, 54615, 56661, 57773, 60093, 60849, 62565, 63486, 64677, 65694, 67217, 68581, 69965, 70978, 72145, 73005, 74901, 75625, 76462, 77245, 78669, 79265, 80810, 82202, 83574, 84964, 85992, 86981, 87828, 88681, 90681, 92781, 94556, 95941, 96749, 100052]
+// var queue=[742, 0822, 0822, 373, 757, 073, 450, 578, 769, 521, 234, 037, 154, 217, 325, 423, 415, 661, 773, 93, 089, 665, 386, 477, 594, 717, 881, 965, 978, 245, 305, 741, 625, 662, 725, 769, 965, 0810, 202, 574, 464, 592, 681, 828, 681, 681, 281, 456, 941, 749, 1052,11,11,11,11]
 for (var i = 0; i <= 54; i++) {
         images[images.length] = new Image();
         images[images.length - 1].src = "./扑克牌图片/downyi.com (" + i + ").jpg";
@@ -50,10 +47,9 @@ var queueTime=setInterval(function(){
     if(queue.length>0 && queueFlag==true)
     {   var time=queue.shift();
         count++;
-        $(".loading>div>div").css({"width":"300","transition":"width "+ time +"ms ease 0s"});
+        $(".loading>div>div").css({"width":""+Math.floor(300/55*count),"transition":"width "+ time +"ms ease 0s"});
         setTimeout(function(){ 
             $(".loading>div>span>span").text(count+"/55");
-            $(".loading>div>div").css({"width":"0","transition":""});
             queueFlag=true;
         },time);
         queueFlag=false;
@@ -61,11 +57,10 @@ var queueTime=setInterval(function(){
     if(count==55){
         clearInterval(queueTime);
         setTimeout(function(){ 
-            $(".loading>div>div").css({"width":"300","transition":""});
             $(".loading>div>span>span").text("完毕");
             setTimeout(function(){
                 $(".loading").remove();
             },100);
-        },time+200);
+        },500);
     }
 },10)
